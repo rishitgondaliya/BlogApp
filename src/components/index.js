d9ae98dcd15770a11552ce8ea218a1098cc73e26
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-06 18:16:10 +0530

    index file for exporting components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import container from "./container/container";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Header/Logo"
+
+export {
+    container,
+    Header,
+    Footer,
+    Logo
+}
