e6d52e217919996b6859bcba5a7527cd45e5bef0
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-10 19:16:56 +0530

    export default components

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,33 @@
-import container from "./container/container";
+import Container from "./container/container";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Logo from "./Header/Logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import Button from "./input/Button";
+import Input from "./input/Input";
+import Select from "./input/Select";
+import BlogCard from "./Blog/BlogCard";
+import RTE from "./RTE/RTE";
+import BlogForm from "./Blog/BlogForm";
+import AuthLayout from '../components/auth/AuthLayout'
+import Login from "./auth/Login";
+import Signup from "./auth/Signup"
+import Profile from "./profile/Profile";
 
 export {
-    container,
+    Container,
     Header,
     Footer,
-    Logo
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    BlogCard,
+    RTE,
+    BlogForm,
+    AuthLayout, 
+    Login,
+    Signup,
+    Profile
 }
