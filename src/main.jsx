27ed253b9357e64b9c040b247cbfcf908ea514a1
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-10 19:10:23 +0530

    setting up routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,91 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { AuthLayout, Profile } from '../src/components'
+import { Login as LoginComponent } from '../src/components'
+import { Signup as SignupComponent } from '../src/components'
+import { HomePage, AddBlog, AllBlogs, BlogPage, EditBlog } from '../src/pages'
 import store from './store/store.js'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '/home',
+          element: <HomePage />
+        },
+        {
+          path: '/login',
+          element: (
+            <AuthLayout authentication={false}>
+              <LoginComponent />
+            </AuthLayout>
+          )
+        },
+        {
+          path: '/signup',
+          element: (
+            <AuthLayout authentication={false}>
+              <SignupComponent />
+            </AuthLayout>
+          )
+        },
+        {
+          path: '/all-blogs',
+          element: (
+            <AuthLayout authentication>
+              <AllBlogs />
+            </AuthLayout>
+          )
+        },
+        {
+          path: '/create-blog',
+          element: (
+            <AuthLayout authentication>
+              <AddBlog />
+            </AuthLayout>
+          )
+        },
+        {
+          path: "/edit-post/:slug",
+          element: (
+            <AuthLayout authentication>
+              {" "}
+              <EditBlog />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: "/profile",
+          element: (
+            <AuthLayout authentication>
+              {" "}
+              <Profile />
+            </AuthLayout>
+          )
+        },
+        {
+          path: "/blog/:slug",
+          element: (
+            <AuthLayout>
+              <BlogPage />
+            </AuthLayout>
+          )
+        }
+      ]
+    }
+  ]
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 )
