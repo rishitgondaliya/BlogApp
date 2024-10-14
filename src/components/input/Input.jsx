a338905dfd9c018c6e3fb9352d6c9c3bad4381e6
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-14 11:36:44 +0530

    created global input

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -11,16 +11,18 @@ const Input = React.forwardRef(function Input({
     return (
         <div className="w-full">
             {label &&
-                <label htmlFor={id} className="inline-block mb-1 pl-1">
+                <label
+                    className="inline-block mb-1 pl-1"
+                    htmlFor={id}>
                     {label}
                 </label>
             }
             <input
                 type={type}
-                id={id}
-                className={`${className}`}
-                {...props}
+                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
                 ref={ref}
+                {...props}
+                id={id}
             />
         </div>
     )
