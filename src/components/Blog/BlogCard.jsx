c0fc05c76d8f448d2046a85a1050610ded12f66d
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-15 19:12:31 +0530

    changed styles

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -2,14 +2,18 @@
 import { Link } from 'react-router-dom'
 import fileUploadService from '../../appwrite/fileUpload'
 
-function BlogCard({$id, title, blogImg}) {
-  return (
+function BlogCard({ $id, title, blogImage }) {
+  const imageUrl = blogImage
+    ? fileUploadService.getFilePreview(blogImage)
+    : "https://static.vecteezy.com/system/resources/previews/036/226/872/non_2x/ai-generated-nature-landscapes-background-free-photo.jpg";
+  
+    return (
     <Link to={`/blog/${$id}`}>
-      <div className="w-full bg-blue-200 rounded-xl p-4">
-        <h2 className='text-2xl text-black font-medium'></h2>
+      <div className="w-full max-w-80 bg-blue-200 rounded-xl p-4">
         <div className="w-full justify-center mb-4">
-          <img src={fileUploadService.getImgPreview(blogImg)} alt={title} className="w-full h-48" />
+          <img src={imageUrl} alt={title} className="rounded-xl" />
         </div>
+        <h2 className='text-xl text-black font-bold'>{title}</h2>
       </div>
     </Link>
   )
