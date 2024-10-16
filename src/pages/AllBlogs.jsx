10f97b247652ca46c2eb3d14978c14da822a1adb
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-16 10:21:05 +0530

    logic & syntax correction

--- src/pages/AllBlogs.jsx
+++ src/pages/AllBlogs.jsx
@@ -1,29 +1,52 @@
 import { useEffect, useState } from "react"
-import appwriteService from "../appwrite/appwriteService"
 import { Container, BlogCard } from "../components"
+import appwriteService from "../appwrite/appwriteService"
 
 function AllBlogs() {
     const [blogs, setBlogs] = useState([])
-    useEffect(() => {}, [])
-    appwriteService.getAllBlogs([])
-    .then((blogs) => {
-        if(blogs) {
-            setBlogs(blogs.documents)
-        }
-    })
-  return (
-    <div className="w-full py-8">
-        <Container>
-            <div className="flex flex-wrap">
-                {blogs.map((blog) => (
-                    <div key={blog.$id} className="p-2 w-1/4">
-                        <BlogCard blog={blog} />
-                    </div>
-                ))}    
-            </div>    
-        </Container>        
-    </div>
-  )
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        appwriteService.getAllBlogs([])
+            .then((response) => {
+                if (response) {
+                    setBlogs(response.documents)
+                }
+                setLoading(false)
+            })
+            .catch(() => {
+                setLoading(false)
+            })
+    }, [])
+
+    if (loading) {
+        return <div className="w-full py-8 mt-4 text-center">
+            <Container>
+                <div className="flex justify-center items-center">
+                    <h1 className="text-xl font-medium">Loading blogs for you...</h1>
+                    <div className="ml-4 animate-spin rounded-full h-8 w-8 border-t-2 border-black"></div>
+                </div>
+            </Container>
+        </div>
+    }
+
+    if (!blogs.length) {
+        return <div className="text-center text-3xl my-8">No blogs available ☹️</div>;
+    }
+
+    return (
+        <div className="w-full py-8">
+            <Container>
+                <div className="flex flex-wrap">
+                    {blogs.map((blog) => (
+                        <div key={blog.$id} className="p-2 w-1/4">
+                            <BlogCard {...blog} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
 }
 
 export default AllBlogs
