b6e8cd46bd036abb915448053a69b876e77388b9
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-17 22:14:18 +0530

    style changed

--- src/components/input/Button.jsx
+++ src/components/input/Button.jsx
@@ -9,7 +9,7 @@ function Button({
   ...props
 }) {
   return (
-    <button className={`px-4 py-2 rounded-lg ${type} ${bgColor} ${textColor} ${className}`} {...props}>
+    <button className={`py-1 px-2 rounded-lg ${type} ${bgColor} ${textColor} ${className}`} {...props}>
       {children}
     </button>
   )
