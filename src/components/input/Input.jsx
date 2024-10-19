1c194d0b6ab37b0ad8935ac333a21ca6e8c95870
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-19 21:30:21 +0530

    used material-ui to design input field

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,31 +1,32 @@
 /* eslint-disable react/prop-types */
-import React, { useId } from "react"
+import React, { useId } from "react";
+import TextField from '@mui/material/TextField';
 
 const Input = React.forwardRef(function Input({
     label,
     type = "text",
     className = "",
+    variant = "outlined",
+    margin = "normal",
+    fullWidth = true,
     ...props
 }, ref) {
-    const id = useId()
+    const id = useId();
+
     return (
-        <div className="w-full">
-            {label &&
-                <label
-                    className="inline-block mb-1 pl-1"
-                    htmlFor={id}>
-                    {label}
-                </label>
-            }
-            <input
+        <div className={`w-full ${className}`}>
+            <TextField
                 type={type}
-                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-                ref={ref}
-                {...props}
                 id={id}
+                label={label}
+                variant={variant}
+                margin={margin}
+                fullWidth={fullWidth}
+                inputRef={ref}
+                {...props}
             />
         </div>
-    )
-})
+    );
+});
 
-export default Input
+export default Input;
