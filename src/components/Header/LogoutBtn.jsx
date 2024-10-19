64d70f7b9007e7e1cd776c3a965107e28a9dc633
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-19 21:27:49 +0530

    used material-ui button

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,24 +1,32 @@
 /* eslint-disable react/prop-types */
 import { useDispatch } from "react-redux";
 import { logout } from '../../store/authSlice';
+import { useNavigate } from "react-router-dom";
+import {Button} from '../index'
 import authService from '../../appwrite/authentication';
 
 export default function LogoutBtn({ onLogout }) {
     const dispatch = useDispatch();
+    const navigate = useNavigate()
     const logoutHandler = () => {
         authService.logout()
             .then(() => {
                 dispatch(logout());
                 if (onLogout) onLogout(); // Call the onLogout prop
+                navigate("/")
             });
     };
 
     return (
-        <button
-            className="inline-block text-base px-6 py-2 duration-200 bg-red-500 hover:bg-[#f9928d] rounded"
+        <Button
+            className="inline-block duration-200 bg-red-500 hover:bg-[#c25c56]"
+            paddingX={2}
+            paddingY={1}
+            bgColor="red"
+            fontSize={'0.9rem'}
             onClick={logoutHandler}
         >
             LOGOUT
-        </button>
+        </Button>
     );
 }
