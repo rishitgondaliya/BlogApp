b4e2b49f9ce9704feb547845731ba96e9f0b0097
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-27 16:26:15 +0530

    design changed

--- src/components/input/Select.jsx
+++ src/components/input/Select.jsx
@@ -1,30 +1,37 @@
 /* eslint-disable react/prop-types */
-import React, { useId } from 'react'
+import React, { useId } from 'react';
+import { FormControl, InputLabel, MenuItem, Select as MuiSelect } from '@mui/material';
 
 function Select({
-    options,
-    label,
-    className,
-    ...props
+  options,
+  label,
+  className,
+  value,
+  onChange,
+  ...props
 }, ref) {
-    const id = useId()
-    return (
-        <div className='w-full'>
-            {label && <label htmlFor={id} className=''></label>}
-            <select
-                {...props}
-                id={id}
-                ref={ref}
-                className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-            >
-                {options?.map((option) => (
-                    <option key={option} value={option}>
-                        {option}
-                    </option>
-                ))}
-            </select>
-        </div>
-    )
+  const id = useId();
+
+  return (
+    <FormControl fullWidth className={className}>
+      {label && <InputLabel id={`${id}-label`}>{label}</InputLabel>}
+      <MuiSelect
+        labelId={`${id}-label`}
+        id={id}
+        value={value}
+        label={label}
+        onChange={onChange}
+        ref={ref}
+        {...props}
+      >
+        {options?.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
+      </MuiSelect>
+    </FormControl>
+  );
 }
 
-export default React.forwardRef(Select) 
+export default React.forwardRef(Select);
