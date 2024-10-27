7d3b8289c46d44b6b6c7046cd91ac8345cee4fc6
Rishit Gondaliya <mrrishitgondaliya@gmail.com>
2024-10-27 16:23:51 +0530

    logo changed

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
-import logo from '../../assets/icon.png'
+import logo from '../../assets/logo/logo.png'
 
-function Logo({width = '100px'}) {
+function Logo({width = '120px'}) {
   return (
     <div>
       <img src={logo} alt="" width={width}/>
